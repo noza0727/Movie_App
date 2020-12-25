5abdcf6e8ffedce144ec3e44b788a6231e49fab7
noza0727 <45297004+noza0727@users.noreply.github.com>
2020-12-26 00:43:15 +0500

    Revert "[ADDED] RecyclerViews for movies and actors"

--- app/src/main/java/com/learning/myapplication/MovieDetailsActivity.kt
+++ app/src/main/java/com/learning/myapplication/MovieDetailsActivity.kt
@@ -1,40 +1,11 @@
 package com.learning.myapplication
 
-import android.content.Intent
-import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.TextView
 
-
-class MovieDetailsActivity : AppCompatActivity(), FragmentMovieList.ClickListener, FragmentMovieDetails.ClickListener {
-
-
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.main_container, FragmentMovieList.newInstance(), FragmentMovieList::class.java.simpleName)
-                .addToBackStack("trans:${FragmentMovieList}")
-                .commit()
-        }
-
-    }
-
-
-    override fun onMovieDetailsClick() {
-        supportFragmentManager.beginTransaction()
-            .add(R.id.main_container, FragmentMovieDetails.newInstance(), FragmentMovieDetails::class.java.simpleName)
-            .addToBackStack("trans:${FragmentMovieDetails}")
-            .commit()
-    }
-
-    override fun onBackToMovieList() {
-        supportFragmentManager.popBackStack()
-
+        setContentView(R.layout.activity_movie_details)
     }
 }
